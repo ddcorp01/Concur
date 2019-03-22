d83746c1026fc30e61d97924ede8d8ad3111df99
Tejas Umesh Shastry <48762726+TejasUmeshShastry@users.noreply.github.com>
2019-03-22 08:00:28 +0530

    added user and project

--- Home/models.py
+++ Home/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+class User(models.Models):
+    userName = models.CharField(max_length=100)
+    userDisc = models.TextField(max_length=300)
+
+    def __str__(self):
+        return self.userName
+    
+class Project(models.Model):
+    projectName = models.CharField(max_lenth=100)
+    projectDisc = models.TextField(max_length=300)
+
+    def __str__(self):
+        return self.projectName
+        
+    
 # Create your models here.
