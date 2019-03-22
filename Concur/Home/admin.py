5e78298fd973aeac09fe86bd803028d8663bf265
Tejas Umesh Shastry <48762726+TejasUmeshShastry@users.noreply.github.com>
2019-03-22 08:49:00 +0530

    fixed nesting error

---
+++ Concur/Home/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import User,Project
+
+admin.site.register(User)
+admin.site.register(Project)
+
+# Register your models here.
--- Concur/Home/models.py
+++ Concur/Home/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class User(models.Models):
+class User(models.Model):
     userName = models.CharField(max_length=100)
     userDisc = models.TextField(max_length=300)
 
@@ -8,11 +8,11 @@ class User(models.Models):
         return self.userName
     
 class Project(models.Model):
-    projectName = models.CharField(max_lenth=100)
+    projectName = models.CharField(max_length=100)
     projectDisc = models.TextField(max_length=300)
 
     def __str__(self):
         return self.projectName
-        
+
     
 # Create your models here.
